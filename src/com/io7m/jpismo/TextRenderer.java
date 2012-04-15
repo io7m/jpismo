3c632f6db024c47b39df99b30cbc88e148c13f56
m0 <m0>
2012-04-15 21:10:30 +0000

    Add text width functions

--- src/com/io7m/jpismo/TextRenderer.java
+++ src/com/io7m/jpismo/TextRenderer.java
@@ -15,7 +15,7 @@ public interface TextRenderer
    * @throws TextCacheException
    *           Iff the given text could not be cached.
    * @throws ConstraintError
-   *           Iff <code>line</code> is null.
+   *           Iff <code>line == null</code>.
    * @throws GLException
    *           Iff an OpenGL error occurs.
    */
@@ -45,4 +45,25 @@ public interface TextRenderer
    */
 
   int getLineHeight();
+
+  /**
+   * Return the number of pixels the given text would take when rendered with
+   * the current renderer.
+   * 
+   * @param line
+   *          The text.
+   * @return The length in pixels.
+   * @throws TextCacheException
+   *           Iff an error occurs whilst caching the input text.
+   * @throws ConstraintError
+   *           Iff <code>line == null</code>.
+   * @throws GLException
+   *           Iff an OpenGL error occurs.
+   */
+
+  int getTextWidth(
+    final @Nonnull String line)
+    throws GLException,
+      ConstraintError,
+      TextCacheException;
 }
